ee246b1a20bd346e7c2edaffc9840b369c9f8943
Tom Sproull <64221818+sprouller@users.noreply.github.com>
2023-03-09 10:52:58 +0000

    update

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation';
 
 import SignOut from 'src/components/SignOut';
 import createClient from 'src/lib/supabase-server';
+import Breaches from 'src/components/Pwned/breach';
 
 export default async function Profile() {
   const supabase = createClient();
@@ -30,6 +31,7 @@ export default async function Profile() {
       <h2>User Profile</h2>
       <code className="highlight">{user.email}</code>
       <code className="highlight">{userData.username}</code>
+      <code className="highlight"><Breaches/></code>
       <div className="heading">Last Signed In:</div>
       <code className="highlight">{new Date(user.last_sign_in_at).toUTCString()}</code>
       <Link className="button" href="/">
